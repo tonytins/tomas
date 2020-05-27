8813586efc47d7e0053bc5cf9d8cae569eb5e8e0
Anthony Foxclaw <noreply@tonytins.xyz>
2020-05-27 13:55:45 -0400

    Formatting changes
    - Update to gitignore
    - Removed nuget.config

--- src/Tomas.Kernel/OSConsts.cs
+++ src/Tomas.Kernel/OSConsts.cs
@@ -1,15 +1,15 @@
-﻿// TOMAS is licensed under the MPL 2.0 license.
+// TOMAS is licensed under the MPL 2.0 license.
 // See the LICENSE file in the project root for more information.
 namespace Tomas.Kernel
 {
- public struct OSConsts
- {
-  /// <summary>
-  /// Name of the operating system
-  /// </summary>
-  public const string NAME = "TOMAS";
+    public struct OSConsts
+    {
+        /// <summary>
+        /// Name of the operating system
+        /// </summary>
+        public const string NAME = "TOMAS";
 
-  public static string Version = $"{ThisAssembly.Git.SemVer.Major}.{ThisAssembly.Git.SemVer.Minor}.{ThisAssembly.Git.SemVer.Patch}";
-  public static string VersionGit = $"{Version}-{ThisAssembly.Git.Commit}";
- }
+        public static string Version = $"{ThisAssembly.Git.SemVer.Major}.{ThisAssembly.Git.SemVer.Minor}.{ThisAssembly.Git.SemVer.Patch}";
+        public static string VersionGit = $"{Version}-{ThisAssembly.Git.Commit}";
+    }
 }
--- src/Tomas.Kernel/Programs/App.cs
+++ src/Tomas.Kernel/Programs/App.cs
@@ -1,30 +1,30 @@
-﻿// TOMAS is licensed under the MPL 2.0 license.
+// TOMAS is licensed under the MPL 2.0 license.
 // See the LICENSE file in the project root for more information.
 using sys = Cosmos.System;
 
 namespace Tomas.Kernel.Programs
 {
- /// <summary>
- /// Basic framework for building terminal applications.
- /// </summary>
- public abstract class App
- {
-  protected App(Kernel system)
-  {
-   System = system;
-  }
+    /// <summary>
+    /// Basic framework for building terminal applications.
+    /// </summary>
+    public abstract class App
+    {
+        protected App(Kernel system)
+        {
+            System = system;
+        }
 
-  Kernel System { get; set; }
+        Kernel System { get; set; }
 
-  /// <summary>
-  /// Main entry point of the program
-  /// </summary>
-  public virtual void Start()
-  {
-   System.InApp = true;
-   var isCKey = sys.KeyboardManager.ReadKey().Key == sys.ConsoleKeyEx.C;
-   if (sys.KeyboardManager.ControlPressed && isCKey)
-    System.Restart();
-  }
- }
+        /// <summary>
+        /// Main entry point of the program
+        /// </summary>
+        public virtual void Start()
+        {
+            System.InApp = true;
+            var isCKey = sys.KeyboardManager.ReadKey().Key == sys.ConsoleKeyEx.C;
+            if (sys.KeyboardManager.ControlPressed && isCKey)
+                System.Restart();
+        }
+    }
 }
