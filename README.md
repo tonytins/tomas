f006ab41bf96cbee6a83f17dbdd2339ce44e72f5
Tony Bark <noreply@tonybark.com>
2023-01-07 12:39:07 -0500

    Issue and pull request templates - Added code of conduct - Code climate badges

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # TOMAS
 
-<img title="" src="screenshot.png" alt="" data-align="center">
+<p align="center">
+  <a href="https://github.com/tonytins/tomas/blob/main/LICENSE"><img src="https://img.shields.io/github/license/tonytins/tomas" alt="GitHub license"></a>
+  <a href="https://github.com/tonytins/tomas/actions?query=workflow%3Adotnet.yml"><img src="https://img.shields.io/github/actions/workflow/status/tonytins/tomas/dotnet.yml" alt="GitHub Workflow Status"></a>
+  <img src="https://img.shields.io/github/commit-activity/w/tonytins/tomas" alt="GitHub commit activity">
+  <br>
+  <a href="code_of_conduct.md"><img src="https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg" alt="Contributor Covenant"></a>
+  <img src="https://img.shields.io/codeclimate/maintainability-percentage/tonytins/tomas" alt="Code Climate maintainability">
+  <img src="https://img.shields.io/codeclimate/tech-debt/tonytins/tomas" alt="Code Climate technical debt">
+</p>
+
+
+<img title="" src="screenshot.png" alt="" align="center">
 
 TOMAS (**To**ny's **Ma**naged Operating **S**ystem) is a hobby operating system based on the [COSMOS](https://github.com/CosmosOS/Cosmos) framework that comes with a respective terminal emulator.
 
--- src/Tomas.Kernel/Shell.cs
+++ src/Tomas.Kernel/Shell.cs
@@ -8,12 +8,12 @@ namespace Tomas.Kernel;
 
 public class Shell : IShell
 {
- // The symbol to display before the cursor when the shell is waiting for user input
- const char SYMBOL = '$';
+    // The symbol to display before the cursor when the shell is waiting for user input
+    const char SYMBOL = '$';
 
- // A dictionary containing the programs available to the shell, with the keys being the program names
- // and the values being the program objects
- public Dictionary<string, IProgram> Programs => new()
+    // A dictionary containing the programs available to the shell, with the keys being the program names
+    // and the values being the program objects
+    public Dictionary<string, IProgram> Programs => new()
     {
         {"about", new About() },
         {"fensay", new FenSay() },
@@ -21,20 +21,20 @@ public class Shell : IShell
         {"commands", new Commands() }
     };
 
- // A property that allows the shell to read a line of input from the user
- public string ReadLine
- {
-  get
-  {
-   // Write the SYMBOL character to the console
-   Console.Write(SYMBOL);
+    // A property that allows the shell to read a line of input from the user
+    public string ReadLine
+    {
+        get
+        {
+            // Write the SYMBOL character to the console
+            Console.Write(SYMBOL);
 
-   // Read a line of input from the user
-   var readl = Console.ReadLine();
+            // Read a line of input from the user
+            var readl = Console.ReadLine();
 
-   // Return the line of input
-   return readl;
-  }
- }
+            // Return the line of input
+            return readl;
+        }
+    }
 }
 
