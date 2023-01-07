e44f7e8f951633b5071ad1a1d9e9db803333fba2
Tony Bark <noreply@tonybark.com>
2023-01-06 20:45:27 -0500

    Implicit usings applied to Tomas.Interface

--- src/Tomas.Interface/IShell.cs
+++ src/Tomas.Interface/IShell.cs
@@ -1,13 +1,11 @@
 // I license this project under the BSD 3-Clause license.
 // See the LICENSE file in the project root for more information.
-using System.Collections.Generic;
 
-namespace Tomas.Interface
+namespace Tomas.Interface;
+
+public interface IShell
 {
- public interface IShell
- {
-  string ReadLine { get; }
+    string ReadLine { get; }
 
-  Dictionary<string, IProgram> Programs { get; }
- }
+    Dictionary<string, IProgram> Programs { get; }
 }
